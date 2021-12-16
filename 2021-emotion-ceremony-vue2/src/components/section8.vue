f087d9bbc9c3d252647c6674d4e18c933c91f7a6
jihyeon <jihyeon.kim@emotion.co.kr>
2021-12-16 11:02:31 +0900

    marquee slide 간격 수정

---
+++ 2021-emotion-ceremony-vue2/src/components/section8.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="df" style="height: 700px; background: palevioletred">
+    <span>dfdf</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "section8"
+}
+</script>
+
+<style lang="scss" scoped>
+  .df {
+    height: 800px;
+    span{
+      font-size: 100px;
+    }
+  }
+</style>
