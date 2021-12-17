3555e5f5ed2e73629f2f8872cf1cbe44af56bc22
jihyeon <jihyeon.kim@emotion.co.kr>
2021-12-17 09:34:09 +0900

    시계 작업 중

--- 2021-emotion-ceremony-vue2/src/components/section7.vue
+++ 2021-emotion-ceremony-vue2/src/components/section7.vue
@@ -2,13 +2,8 @@
   <!-- https://github.com/philipjkim/vue2-flip-countdown -->
   <div class="count-wrap">
     <div>
-      나의 파이트 넘버 :
-      <span style="color: #ff0000;">85</span>
-    </div>
-    <div>
       <!-- parameters :showDays="false" -->
       <flip-countdown deadline="2021-12-23 00:00:00" :showDays="false"></flip-countdown>
-      <span>후 배틀 시작</span>
     </div>
   </div>
 </template>
@@ -27,8 +22,13 @@ export default {
 
 <style lang="scss">
 .count-wrap {
-  .flip-card {
-    font-size: 150px;
+  .flip-clock__piece {
+    width: 380px;
+  }
+  .flip-clock__card.flip-card {
+    font-size: 200px;
+    font-family: "HelveticaNeue-Medium";
+    letter-spacing: 0;
   }
 }
 
